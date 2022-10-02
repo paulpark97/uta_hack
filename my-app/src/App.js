0e266367a2770a2ce4846d25efbc00c3c5dab880
paulpark97 <paulpark97@yahoo.com>
2022-10-02 02:58:57 -0500

    mvp

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import Home from './Screens/Home.js';
-import LifeChoice from './Screens/LifeChoice.js';
-import Profession from './Screens/Profession.js';
-import Smoke from './Screens/Smoke.js';
-import Alcohol from './Screens/Alcohol.js';
-import Drugs from './Screens/Drugs.js';
-import SecondScreen from './Screens/SecondScreen.js';
+import bbq from './assets/bbq.jpg';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./utilities/Header.js";
 import Slides, {GRADIENT_COLOR} from './Screens/components/Slides';
@@ -23,8 +18,8 @@ const DEATH_YES_NO = [
   {title:'No',link:'/smoke'}
 ]
 const SMOKE_YES_NO = [
-  {title:'Yes',link:'/smoke'},
-  {title:'No',link:'/smoke'}
+  {title:'Yes',link:'/smoke_pic'},
+  {title:'No',link:'/smoke_pic'}
 ]
 function App() {
   return (
@@ -36,6 +31,7 @@ function App() {
           <Route path='/profession' element={<OptionsSlides backgroundColor={GRADIENT_COLOR} title="What is your profession?" data={PROFESSION_OPTIONS}/>}/>
           <Route path='/death' element={<OptionsSlides backgroundColor={GRADIENT_COLOR} title="Have you eveer had a near death experince?" data={DEATH_YES_NO}/>}/>
           <Route path='/smoke' element={<OptionsSlides backgroundColor={GRADIENT_COLOR} title="Do you smoke?" data={SMOKE_YES_NO}/>}/>
+          <Route path='/smoke_pic' element={<OptionsSlides backgroundColor={GRADIENT_COLOR} title="Smokers are 3 times more likely to die earlier" img={bbq}/>}/>
           
           
           <Route path='/slides' element={<Slides color={GRADIENT_COLOR}/>}/>
