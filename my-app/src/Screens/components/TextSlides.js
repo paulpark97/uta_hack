00e71e27229b9c0eb66da1dba0fbe0356f723a26
phuong linh nguyen <linh.nguyen14@mavs.uta.edu>
2022-10-02 03:37:31 -0500

    some

--- my-app/src/Screens/components/TextSlides.js
+++ my-app/src/Screens/components/TextSlides.js
@@ -1,9 +1,11 @@
 import Slides from "./Slides";
+import styles from "../../styles/slide.module.css";
 
 export default function ({backgroundColor, color, content}) {
     return (
         <Slides color={backgroundColor}>
-            <p>{content}</p>
+            <p className={styles.textSlides}>{content}</p>
+
         </Slides>
     )
 }
