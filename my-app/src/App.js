8af836cba57808834deb052a5e8c01a4aaa8b0d5
paulpark97 <paulpark97@yahoo.com>
2022-10-02 02:50:32 -0500

    mvp

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,25 +23,21 @@ const DEATH_YES_NO = [
   {title:'No',link:'/smoke'}
 ]
 const SMOKE_YES_NO = [
-  {title:'Yes',link:'/smoke_pic'},
-  {title:'No',link:'/smoke_pic'}
+  {title:'Yes',link:'/smoke'},
+  {title:'No',link:'/smoke'}
 ]
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path='/' element={<Home/>} />
           <Route path='/textSlides' element={<TextSlides backgroundColor={GRADIENT_COLOR} content="People’s lives are always at risk, the below riskometer measure’s an individual’s probability of passing due to their life choices"/>}/>
           <Route path='/profession' element={<OptionsSlides backgroundColor={GRADIENT_COLOR} title="What is your profession?" data={PROFESSION_OPTIONS}/>}/>
-          <Route path='/death' element={<OptionsSlides backgroundColor={GRADIENT_COLOR} title="Have you eveer had a newar death experince?" data={DEATH_YES_NO}/>}/>
+          <Route path='/death' element={<OptionsSlides backgroundColor={GRADIENT_COLOR} title="Have you eveer had a near death experince?" data={DEATH_YES_NO}/>}/>
           <Route path='/smoke' element={<OptionsSlides backgroundColor={GRADIENT_COLOR} title="Do you smoke?" data={SMOKE_YES_NO}/>}/>
-          <Route path='/smoke_pic' element={<TextSlides backgroundColor={GRADIENT_COLOR} content="Smokers are 3 times more likely to die earlier"/>}/>
-          <Route path='/life' element={<LifeChoice/>}/>
-          <Route path='/pro' element={<Profession/>}/>
-          <Route path='/smoke' element={<Smoke/>}/>
-          <Route path='/alc' element={<Alcohol/>}/>
-          <Route path='/drugs' element={<Drugs/>}/>
-          <Route path='/second' element={<SecondScreen/>}/>
+          
+          
           <Route path='/slides' element={<Slides color={GRADIENT_COLOR}/>}/>
         </Routes>
       </Router>
--- my-app/src/Screens/components/OptionsSlides.js
+++ my-app/src/Screens/components/OptionsSlides.js
@@ -4,12 +4,12 @@ import Options from "./Options";
 
 import styles from "../../styles/slide.module.css";
 
-export default function ({backgroundColor, color, content, title, data}) {
+export default function ({backgroundColor, color, content, title, data,img}) {
     return (
         <Slides color={backgroundColor}>
             <div className={`${styles.optionsContent}`}>
                 <SlideTitle title={title}/>
-                <Options data={data}/>
+                {(data)? <Options data={data}/>:<img src={img}/>}
             </div>
         </Slides>
     )
